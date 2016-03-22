b4389e239c07a8eaf0073fd922678b1a0b84853b
Oles Onyshchak <oles1719@gmail.com>
2016-03-23 01:03:18 +0200

    Initial commit

---
+++ src/main/java/com/boot/repository/HomeController.java
@@ -0,0 +1,12 @@
+package com.boot.repository;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "Hi Spring Boot";
+    }
+}
